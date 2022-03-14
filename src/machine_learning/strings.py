fd00c2ca82e6da2d9295480a072f4b6d3ef492e6
Dinh Huu Nguyen <dnguyen@cylance.com>
2022-03-13 23:45:40 -0700

    add some often-repeated strings

---
+++ src/machine_learning/strings.py
@@ -0,0 +1,45 @@
+encoder_str = "encoder"
+decoder_str = "decoder"
+
+# activations
+activation_name_str = "activation_name"
+identity_str = "identity"
+leaky_relu_str = "leaky_relu"
+relu_str = "relu_str"
+sigmoid_str = "sigmoid"
+softmax_str = "softmax"
+swish_str = "swish"
+tanh_str = "tanh"
+
+# losses
+bce_loss_str = "bce_loss"
+ce_loss_str = "ce_loss"
+kl_div_loss_str = "kl_div_loss"
+l1_loss_str = "l1_loss"
+mse_loss_str = "mse_loss"
+nll_loss_str = "nll_loss"
+
+# optimizers
+sgd_str = "sgd"
+adam_str = "adam"
+adamw_str = "adamw"
+rmsprop_str = "rmsprop"
+
+# regularizers
+l1_str = "l1"
+l2_str = "l2"
+
+# weights initializers
+normal_str = "normal"
+ones_str = "ones"
+small_str = "small"
+uniform_str = "uniform"
+xavier_normal_str = "xavier_normal"
+xavier_uniform_str = "xavier_uniform"
+
+n_factors_str = "n_factors"
+n_features_str = "n_features"
+state_dict_str = "state_dict"
+
+not_implemented_str = "not_implemented"
+same_str = "same"
